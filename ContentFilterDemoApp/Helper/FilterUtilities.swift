ed1a6269795a36ba77e9af97d6082ab045bfc989
Netanelz525 <130410270+Netanelz525@users.noreply.github.com>
2025-11-18 14:35:07 +0200

    Update remediation button text in FilterUtilities to improve clarity and user experience. Adjust User Interface State in Xcode project workspace to reflect recent changes.

--- ContentFilterDemoApp/Helper/FilterUtilities.swift
+++ ContentFilterDemoApp/Helper/FilterUtilities.swift
@@ -9,7 +9,7 @@ open class FilterUtilities {
     public static let defaults = UserDefaults(suiteName: "7J3EXH6427.group.com.demo.ContentFilterDemoApp")
     public static let remediationURLMapKey = "blockedContentURL"
     public static let remediationButtonMapKey = "blockedContentButton"
-    public static let remediationButtonText = "hello שלום"
+    public static let remediationButtonText = "Give me access"
     public static let remediationURL = "https://kfilter-redirecter.vercel.app/api/blocked?blockedUrl=\(NEFilterProviderRemediationURLFlowURL)"
     open class func shouldAllowAccess(_ flow: NEFilterFlow) -> Bool {
         let hostname = FilterUtilities.getFlowHostname(flow)
